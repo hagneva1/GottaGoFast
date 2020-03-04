4ca6267a59059689c34e6e032c7ad79461292bd0
BaptisteCraquelin <baptiste.craquelin76@gmail.com>
2020-03-04 21:01:31 +0100

    add pause / resume buttons

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -10,44 +10,9 @@ export class Menu extends Phaser.Scene {
 
   create () {
     this.scene.moveUp();  	
-    const COLOR_PRIMARY = 0x4e342e;
-    const COLOR_LIGHT = 0x7b5e57;
-    const COLOR_DARK = 0x260e04;
 
 
-  	//this.resumeButton = this.add.text(config.width/2, config.height/2, 'Resume',{ background : #fff});
-    this.resumeButton = this.scene.rexUI.add.label({
-    // x: 0,
-    // y: 0,
-    // anchor: undefined,
-    // width: undefined,
-    // height: undefined,
-
-    orientation: 0,
-
-    background: backgroundGameObject,
-    icon: iconGameObject,
-    iconMask: false,
-    text: textGameObject,
-    expandTextWidth: false,
-    expandTextHeight: false,
-    action: actionGameObject,
-    actionMask: false,
-    align: undefined,
-
-    space: {
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-
-        icon: 0,
-        text: 0,
-    },
-
-    // name: '',
-    // draggable: false
-});
+  	this.resumeButton = this.pauseImg = this.physics.add.sprite(config.width/1.2, config.height/10, 'resume');
     this.resumeButton.setInteractive();
 
     this.resumeButton.on('pointerdown', function() {
