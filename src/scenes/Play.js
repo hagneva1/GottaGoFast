9c9ce174615b42a61164fb5b8e37c0f75d39828c
BaptisteCraquelin <baptiste.craquelin76@gmail.com>
2020-03-04 13:32:08 +0100

    Menu transparent

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -60,19 +60,19 @@ export class Play extends Phaser.Scene {
     // checking for input
     //this.input.on("pointerdown", this.jumpListener, this);
 
-    var pause_label = this.add.text(800 - 100, 20, 'Pause', { font: '24px Arial', fill: '#fff' });
-    pause_label.inputEnabled = true;
+    this.pause_label = this.add.text(800 - 100, 20, 'Pause', { font: '24px Arial', fill: '#fff' });
+    this.pause_label.inputEnabled = true;
     var cursors = this.input.keyboard.createCursorKeys();
-    pause_label.setInteractive();
+    this.pause_label.setInteractive();
 
     var w = 800, h = 600;
-    this.input.on("gameobjectdown", function () {
+    this.pause_label.on("pointerdown", function () {
         
         // When the paus button is pressed, we pause the game
         this.paused = true;
         
-        this.scene.pause()
-        this.scene.switch('Menu');
+        this.scene.pause();
+        this.scene.launch('Menu');
        
     }, this);
 
@@ -81,12 +81,6 @@ export class Play extends Phaser.Scene {
   update() {
     character.jump(this);
 
-    /*pause
-    if(bouton pause)
-        this.scene.launch('sceneB')
-        this.scene.pause();
-
-    */
 
     if (this.player.body.touching.down && character.jumpTimer == 0) {
       character.resetJump();
