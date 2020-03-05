4b2855cc131ace8fc8e9f1dd96ec596665a7eb79
hagneva1 <hagneva1@di-722-16.iut.univ-lehavre.fr>
2020-03-05 16:01:12 +0100

    Add localstorage of bestscore

--- src/entity/score.js
+++ src/entity/score.js
@@ -4,11 +4,15 @@ import config from "../config/parameters";
 export default {
     create(game){
         this.score = 0;
-        game.scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+        this.bestScore = localStorage.getItem("bestScore");
+        if (this.bestScore == null) {
+            this.bestScore = 0;
+        }
+        game.scoreText = game.add.text(16, 16, 'score: 0 (best : ' + this.bestScore + ')', { fontSize: '32px', fill: '#000' });
     },
 
     timeScore: function(game){
         this.score += 1;
-        game.scoreText.text = "score: " + this.score;
+        game.scoreText.text = "score: " + this.score + ' (best : ' + this.bestScore + ')';
     }
  }
